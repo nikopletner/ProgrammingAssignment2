ec2f614088f0a7002086d64a8195a409c1de03d0
nikopletner <n.pletner@gmail.com>
2015-07-25 13:27:09 +0300

    test modifying

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function for Caching the Inverse of a Matrix
 
 ## Write a short comment describing this function
 
